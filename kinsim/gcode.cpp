0946f73dea51a92ff5ea0c200b0295ee7ca6e31d
Rene Hopf <renehopf@mac.com>
2013-11-15 03:33:42 +0100

    parser

--- kinsim/gcode.cpp
+++ kinsim/gcode.cpp
@@ -6,7 +6,7 @@
 //  Copyright (c) 2013 Rene. All rights reserved.
 //
 
-//TODO no newline at end, leerzeilen
+//TODO leerzeilen
 //idee: gcode in path, zeile in vec, vorschau der aktuellen zeile
 
 #include <iostream>
@@ -17,52 +17,50 @@
 using namespace std;
 
 namespace g{
-    enum{G,code,posx,posy,posz,posa,posb,posc}next;
+    enum{any,code,pos}next;
+    enum{x,y,z}axis;
 }
 
 struct path* gcode(const char *filename){
     FILE *f = fopen(filename, "r");
     char c;
-    string word = "";
+    string word;
     struct path* result = 0;
     struct vec newvec;
     float x = 0,y = 0,z = 0;
+    bool linedone;
     
-    g::next = g::G;
+    word.clear();
+    g::next = g::any;
     
     while (!feof(f)) {
         c = fgetc(f);
-
         switch (g::next) {
             case g::code:
                 if(isdigit(c)){
                     word.append(&c,1);
                 }else{
-                    g::next = g::G;
+                    g::next = g::any;
                 }
                 break;
-            case g::posx:
+            case g::pos:
                 if(isdigit(c) || c == '.' || c == '-'){
                     word.append(&c,1);
                 }else{
-                    x = stof(word);
-                    g::next = g::G;
-                }
-                break;
-            case g::posy:
-                if(isdigit(c) || c == '.' || c == '-'){
-                    word.append(&c,1);
-                }else{
-                    y = stof(word);
-                    g::next = g::G;
-                }
-                break;
-            case g::posz:
-                if(isdigit(c) || c == '.' || c == '-'){
-                    word.append(&c,1);
-                }else{
-                    z = stof(word);
-                    g::next = g::G;
+                    switch (g::axis) {
+                        case g::x:
+                            x = stof(word);
+                            break;
+                        case g::y:
+                            y = stof(word);
+                            break;
+                        case g::z:
+                            z = stof(word);
+                            break;
+                        default:
+                            break;
+                    }
+                    g::next = g::any;
                 }
                 break;
             default:
@@ -72,31 +70,52 @@ struct path* gcode(const char *filename){
         switch (c) {
             case 'g':
                 g::next = g::code;
-                word = "";
+                word.clear();
+                linedone = false;
                 break;
             case 'x' :
-                g::next = g::posx;
-                word = "";
+                g::next = g::pos;
+                g::axis = g::x;
+                word.clear();
+                linedone = false;
                 break;
             case 'y' :
-                g::next = g::posy;
-                word = "";
+                g::next = g::pos;
+                g::axis = g::y;
+                word.clear();
+                linedone = false;
                 break;
             case 'z' :
-                g::next = g::posz;
-                word = "";
+                g::next = g::pos;
+                g::axis = g::z;
+                word.clear();
+                linedone = false;
                 break;
             case '\n' :
-                newvec.type = vec::axis;
-                newvec.axis_pos[0] = x;
-                newvec.axis_pos[1] = y;
-                newvec.axis_pos[2] = z;
-                append(&result, newvec);
-                cout << "goto:" << x << " " << y << " " << z << endl;
+                if(!linedone){
+                    newvec.type = vec::axis;
+                    newvec.axis_pos[0] = x;
+                    newvec.axis_pos[1] = y;
+                    newvec.axis_pos[2] = z;
+                    append(&result, newvec);
+                    word.clear();
+                    linedone = true;
+                    cout << "goto:" << x << " " << y << " " << z << endl;
+                }
                 break;
             default:
                 break;
         }
+    }
+    
+    if(word.length() > 0){
+        newvec.type = vec::axis;
+        newvec.axis_pos[0] = x;
+        newvec.axis_pos[1] = y;
+        newvec.axis_pos[2] = z;
+        append(&result, newvec);
+        word.clear();
+        cout << "goto:" << x << " " << y << " " << z << endl;
     }
     return result;
 }
