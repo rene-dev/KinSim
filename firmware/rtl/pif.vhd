60edc7a5aaf10391a5c8623318438df8b2d3426c
crinq <crinq@crinqbook.fritz.box>
2013-11-29 19:48:01 +0100

    pins

--- firmware/rtl/pif.vhd
+++ firmware/rtl/pif.vhd
@@ -13,22 +13,22 @@ entity pif is
 		J1_5      : in std_logic; -- SCL
 
 		-- SPI
-		J1_19      : in std_logic; -- MOSI
-		J1_21      : in std_logic; -- MISO
-		J1_23      : in std_logic; -- SCLK
-		J1_24      : in std_logic; -- CE0
-		J1_26      : in std_logic; -- CE0
+		J1_19      : in std_logic; -- MOSI -- bit 3
+		J1_21      : in std_logic; -- MISO -- bit 2
+		J1_23      : in std_logic; -- SCLK -- bit 4
+		J1_24      : in std_logic; -- CE0 -- bit 1
+		J1_26      : in std_logic; -- CE0 -- bit 0
 
 		-- J1
-		J1_7       : in std_logic;
+		J1_7       : in std_logic; -- bit 5
 		J1_8       : in std_logic;
 		J1_10       : in std_logic;
-		J1_11       : in std_logic;
+		J1_11       : in std_logic; -- bit 6
 		J1_12       : in std_logic;
 		J1_13       : in std_logic;
-		J1_15       : in std_logic;
-		J1_16       : in std_logic;
-		J1_18       : in std_logic;
+		J1_15       : in std_logic; -- bit 7
+		J1_16       : in std_logic; -- bit rw
+		J1_18       : in std_logic; -- bit strobe
 		J1_22       : in std_logic;
 
 		-- J2
@@ -86,6 +86,8 @@ architecture rtl of pif is
 	constant OSC_STR  : string  := "44.33";
 	attribute nom_freq : string;
 	attribute nom_freq of oscinst0 : label is OSC_STR;
+    
+    signal data : std_logic_vector(7 downto 0);
 
 begin
 	OSCInst0: osch
@@ -97,9 +99,13 @@ begin
       osc      => clk,
 			sedstdby => open
 		);
-	pwm1:entity pwm port map(clk,10,LEDR);
+    
+    data <= (J1_15, J1_11, J1_7, J1_23, J1_19, J1_21, J1_24, J1_26);
+        
+        
+
+	pwm1:entity pwm port map(clk,data,LEDR);
 	pwm2:entity pwm port map(clk,30,LEDG);
-	--LEDR <= '0';
-	--LEDG <= '1';
+
 
 end rtl;
